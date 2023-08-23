8070d55aa12fcbb056640d1a3f7200b3872ca51c
Glenn Chiang <glennchiang24@gmail.com>
2023-08-23 22:32:40 +0800

    Intergrated firebase backend with telegram bot Implemented error handling for telegram bot

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -1,15 +1,50 @@
 from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext, ConversationHandler
+from telegram.ext import CallbackContext, ConversationHandler, CommandHandler, MessageHandler, filters
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-async def registerHandler(update: Update, context: CallbackContext):
+BASE_URL = os.getenv('BASE_URL')
+
+
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text(text='Enter your callsign/name')
+    return 'register'
+
+
+async def register(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
-    # request to register user
-    await update.message.reply_text('Registration successful! Type "/clock <distance>" to clock distance in km\n\ne.g. /clock 2.4')
+    callsign = update.message.text
+    try:
+        await update.message.reply_text('Processing...')
+        requests.post(
+            f'{BASE_URL}/registerUser?userId={user_id}&callsign={callsign}')
+        await update.message.reply_text('Registration successful! Type "/clock <distance>" to clock distance in km\n\ne.g. /clock 2.4')
+    except Exception as error:
+        await update.message.reply_text(f'ERROR: {error}')
+
+registerHandler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+    states={
+        'register': [MessageHandler(filters=filters.TEXT, callback=register)]
+    },
+    fallbacks=[]
+)
+
 
 async def clockMileageHandler(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
+    
+    if len(context.args) < 1:
+        await update.message.reply_text('Please specify the distance clocked in km\n\ne.g. /clock 2.4')
+        return
+    
     mileage = context.args[0]
-    # requests.post()
-    total_mileage = 10
-    await update.message.reply_text(f'Mileage clocked! Total mileage: {total_mileage}')
+    
+    try:
+        total_mileage = requests.post(
+            f'{BASE_URL}/addMileage?userId={user_id}&mileage={mileage}').json()['totalMileage']
+        await update.message.reply_text(f'Mileage clocked! Your total mileage so far: {total_mileage}km')
+    except Exception as error:
+        await update.message.reply_text(f'ERROR: {error}')
--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -4,26 +4,26 @@ import json
 import os
 import asyncio
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext, ConversationHandler
+from telegram.ext import Application, CommandHandler
 from dotenv import load_dotenv
 load_dotenv()
 
 
-async def main(event):
+def main():
     app = Application.builder().token(token=os.getenv('BOT_TOKEN')).build()
 
-    app.add_handler(handler=CommandHandler('start', registerHandler))
-    app.add_handler(handler=clockMileageHandler('clock', clockMileageHandler))
+    app.add_handler(handler=registerHandler)
+    app.add_handler(handler=CommandHandler('clock', clockMileageHandler))
 
-    await app.initialize()
-    update = Update.de_json(data=json.loads(event['body']), bot=app.bot)
-    await app.process_update(update=update)
-
-
+    # await app.initialize()
+    # update = Update.de_json(data=json.loads(event['body']), bot=app.bot)
+    # await app.process_update(update=update)
+    print('Bot running...')
+    app.run_polling()
 
 def lambda_handler(event, context):
     asyncio.run(main(event))
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
