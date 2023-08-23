710d28834b0ef9b4f8e597300ca937a06e8e4cc9
Glenn Chiang <glennchiang24@gmail.com>
2023-08-23 21:57:26 +0800

    Wrote functions to registerUser and addMileage

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,13 +3,31 @@ const { onRequest } = require("firebase-functions/v2/https");
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 
 const { initializeApp } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
+const { getFirestore, FieldValue } = require("firebase-admin/firestore");
 
 initializeApp();
 
 const db = getFirestore();
 
-const addDistance = onRequest(async (req, res) => {
-  const { callsign, distance } = req.query
-  db.document("users", callsign)
-})
+exports.registerUser = onRequest(async (req, res) => {
+  const { userId, callsign } = req.query;
+  const userDoc = db.collection("users").doc(userId);
+  const result = await userDoc.set({
+    userId,
+    callsign,
+    totalMileage: 0,
+  });
+  res.json(result);
+});
+
+exports.addMileage = onRequest(async (req, res) => {
+  const { userId, mileage } = req.query;
+  const userDoc = db.collection("users").doc(userId);
+  await userDoc.update({
+    totalMileage: FieldValue.increment(Number(mileage)),
+  });
+  const userDocSnap = await userDoc.get();
+  const userData = userDocSnap.data();
+  const totalMileage = userData.totalMileage;
+  res.json({totalMileage});
+});
