fc75d6b1b07e37f64164d4e2c2884e438f977bbd
Glenn Chiang <glennchiang24@gmail.com>
2023-08-24 09:09:48 +0800

    Fixed backend. No clue why it didn't work before and why it's working now. Fuck you firebase

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,6 +1,5 @@
-const { logger } = require("firebase-functions");
+/* eslint-disable object-curly-spacing */
 const { onRequest } = require("firebase-functions/v2/https");
-const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
@@ -10,17 +9,21 @@ initializeApp();
 const db = getFirestore();
 
 exports.registerUser = onRequest(async (req, res) => {
-  const { userId, callsign } = req.query;
-  const userDoc = db.collection("users").doc(callsign);
-  const result = await userDoc.set({
-    userId,
-    callsign,
-    totalMileage: 0,
-  });
-  res.json(result);
+  try {
+    const { userId, callsign } = req.query;
+    const userDoc = db.collection("users").doc(userId);
+    const result = await userDoc.set({
+      userId,
+      callsign,
+      totalMileage: 0,
+    });
+    res.json(result);
+  } catch (error) {
+    res.json({ error: error });
+  }
 });
 
-exports.addMileage = onRequest(async (req, res) => {
+exports.clockMileage = onRequest(async (req, res) => {
   const { userId, mileage } = req.query;
   const userDoc = db.collection("users").doc(userId);
   await userDoc.update({
@@ -29,5 +32,5 @@ exports.addMileage = onRequest(async (req, res) => {
   const userDocSnap = await userDoc.get();
   const userData = userDocSnap.data();
   const totalMileage = userData.totalMileage;
-  res.json({totalMileage});
+  res.json({ totalMileage });
 });
