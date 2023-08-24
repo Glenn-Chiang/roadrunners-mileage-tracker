2f9438c26edf01fb4a568ecd7c2e37cba173245b
Glenn Chiang <glennchiang24@gmail.com>
2023-08-24 11:42:09 +0800

    Added conversation handler to get teamId from user Added command handlers for viewiing individual and team ranks

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -1,32 +1,42 @@
 from telegram import Update
 from telegram.ext import CallbackContext, ConversationHandler, CommandHandler, MessageHandler, filters
 import requests
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-BASE_URL = os.getenv('BASE_URL')
 
 
-async def start(update: Update, context: CallbackContext):
+async def getCallsign(update: Update, context: CallbackContext):
     await update.message.reply_text(text='Enter your callsign/name')
+    return 'team'
+
+
+async def getTeam(update: Update, context: CallbackContext):
+    context.user_data['callsign'] = update.message.text.upper()
+    await update.message.reply_text(text='Enter your team e.g. A')
     return 'register'
 
 
 async def register(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
-    callsign = update.message.text
+    callsign = context.user_data['callsign']
+    teamId = update.message.text
+
+    if teamId.upper() not in ['A', 'B', 'C']:
+        await update.message.reply_text('Invalid team')
+        return 'team'
+
     try:
         await update.message.reply_text('Processing...')
         requests.post(
-            f'{BASE_URL}/registerUser?userId={user_id}&callsign={callsign}')
+            f'https://registeruser-znvjhrzzxa-uc.a.run.app/?userId={user_id}&callsign={callsign}&teamId={teamId}')
         await update.message.reply_text('Registration successful! Type "/clock <distance>" to clock distance in km\n\ne.g. /clock 2.4')
     except Exception as error:
         await update.message.reply_text(f'ERROR: {error}')
+    return ConversationHandler.END
+
 
 registerHandler = ConversationHandler(
-    entry_points=[CommandHandler('start', start)],
+    entry_points=[CommandHandler('start', getCallsign)],
     states={
+        'team': [MessageHandler(filters=filters.TEXT, callback=getTeam)],
         'register': [MessageHandler(filters=filters.TEXT, callback=register)]
     },
     fallbacks=[]
@@ -35,16 +45,47 @@ registerHandler = ConversationHandler(
 
 async def clockMileageHandler(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
-    
+
     if len(context.args) < 1:
         await update.message.reply_text('Please specify the distance clocked in km\n\ne.g. /clock 2.4')
         return
-    
+
     mileage = context.args[0]
-    
+
     try:
         total_mileage = requests.post(
-            f'{BASE_URL}/addMileage?userId={user_id}&mileage={mileage}').json()['totalMileage']
+            f'https://clockmileage-znvjhrzzxa-uc.a.run.app/?userId={user_id}&mileage={mileage}').json()['totalMileage']
         await update.message.reply_text(f'Mileage clocked! Your total mileage so far: {total_mileage}km')
     except Exception as error:
+        print(error)
+        await update.message.reply_text(f'ERROR: {error}')
+
+
+async def rankHandler(update: Update, context: CallbackContext):
+    try:
+        users = requests.get(f'https://getranking-znvjhrzzxa-uc.a.run.app').json()
+        formatted_users = ('\n').join(
+            [f"{idx + 1}. {user['callsign']} {user['totalMileage']}km" for idx, user in enumerate(users)])
+        await update.message.reply_text(formatted_users)
+    except Exception as error:
+        print(error)
+        await update.message.reply_text(f'ERROR: {error}')
+
+
+async def teamRankHandler(update: Update, context: CallbackContext):
+    BASE_URL = ''
+    try:
+        teams_list = []
+        teams = requests.get(f'{BASE_URL}').json()
+        for team in teams:
+            team_id: str = team['id']
+            team_members: list = team['members']
+            formatted_team_members = ('\n').join(
+                [f"{idx + 1}. {user['callsign']} {user['totalMileage']}km" for idx, user in enumerate(team_members)])
+            formatted_team = f"TEAM {team_id}\n{formatted_team_members}"
+            teams_list.append(formatted_team)
+        formatted_teams_list = ('\n\n').join(teams_list)
+        await update.message.reply_text(formatted_teams_list)
+    except Exception as error:
+        print(error)
         await update.message.reply_text(f'ERROR: {error}')
--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,5 +1,4 @@
-from handlers import registerHandler, clockMileageHandler
-import requests
+from handlers import registerHandler, clockMileageHandler, rankHandler, teamRankHandler
 import json
 import os
 import asyncio
@@ -9,21 +8,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def main():
+async def main(event):
     app = Application.builder().token(token=os.getenv('BOT_TOKEN')).build()
 
-    app.add_handler(handler=registerHandler)
-    app.add_handler(handler=CommandHandler('clock', clockMileageHandler))
+    app.add_handler(registerHandler)
+    app.add_handler(CommandHandler('clock', clockMileageHandler))
+    app.add_handler(CommandHandler('rank', rankHandler))
+    app.add_handler(CommandHandler('teamrank', teamRankHandler))
 
-    # await app.initialize()
-    # update = Update.de_json(data=json.loads(event['body']), bot=app.bot)
-    # await app.process_update(update=update)
-    print('Bot running...')
-    app.run_polling()
+    await app.initialize()
+    update = Update.de_json(data=json.loads(event['body']), bot=app.bot)
+    await app.process_update(update=update)
+
 
 def lambda_handler(event, context):
     asyncio.run(main(event))
 
+
+# def main():
+#     app = Application.builder().token(token=os.getenv('BOT_TOKEN')).build()
+
+#     app.add_handler(handler=registerHandler)
+#     app.add_handler(handler=CommandHandler('clock', clockMileageHandler))
+
+#     print('Bot running...')
+#     app.run_polling()
 
 if __name__ == "__main__":
     main()
